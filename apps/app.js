4b12c09c9b8443ebcb8e96197dc2e8d0ca70983c
aungureanu614 <aungureanu614@gmail.com>
2016-08-02 14:17:40 -0700

    apps/

---
+++ apps/app.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('#search-term').submit(function(event) {
+        event.preventDefault();
+
+        var request = gapi.client.youtube.search.list({
+            q: $('#query').val(),
+            part: 'snippet',
+            type: "video"
+        });
+        request.execute(function(response) {
+            var results = response.result;
+            showResults(results);
+        });
+    });
+});
+
+function showResults(results) {
+    $.each(results.items, function(index, item) {
+        $('#search-results').append("<iframe src=//www.youtube.com/embed/" + item.id.videoId + "></iframe>");
+    });
+
+}
+
+function init() {
+    gapi.client.setApiKey('AIzaSyAAeEliIWNKfAlOjtTFoEfGoTtqt_RAF0I');
+    gapi.client.load("youtube", "v3", function() {
+        //yt api is ready
+    });
+}
